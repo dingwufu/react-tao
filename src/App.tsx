84396c9eb27d3a01e7b02756d614d66ada197d9d
dingwufu <1571516625@qq.com>
2019-01-07 23:45:56 +0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// import Home from './routes/home/Index';
+import Home from './routes/home/Index';
 // import User from './routes/user/Index';
 // import Detail from './routes/detail/Index';
 // import Seat from './routes/seat/Index';
@@ -10,15 +10,14 @@ import './App.scss';
 class App extends React.Component {
   render() {
     return (
-      // <Router>
-      //   <Switch>
-      //     <Route path="/" exact={true} component={Home} />
-      //     <Route path="/user" exact={true} component={User} />
-      //     <Route path="/detail" exact={true} component={Detail} />
-      //     <Route path="/seat" exact={true} component={Seat} />
-      //   </Switch>
-      // </Router>
-      <div>1</div>
+      <Router>
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          {/* <Route path="/user" exact={true} component={User} />
+          <Route path="/detail" exact={true} component={Detail} />
+          <Route path="/seat" exact={true} component={Seat} /> */}
+        </Switch>
+      </Router>
     );
   }
 }
