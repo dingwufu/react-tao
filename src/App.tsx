785dde4241866849d5c59672301d21e523b3ffa1
dingwufu <1571516625@qq.com>
2019-01-07 23:42:26 +0800

    deploy test

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Home from './routes/home/Index';
-import User from './routes/user/Index';
-import Detail from './routes/detail/Index';
-import Seat from './routes/seat/Index';
+// import Home from './routes/home/Index';
+// import User from './routes/user/Index';
+// import Detail from './routes/detail/Index';
+// import Seat from './routes/seat/Index';
 import './App.scss';
 // import logo from './logo.svg';
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route path="/" exact={true} component={Home} />
-          <Route path="/user" exact={true} component={User} />
-          <Route path="/detail" exact={true} component={Detail} />
-          <Route path="/seat" exact={true} component={Seat} />
-        </Switch>
-      </Router>
+      // <Router>
+      //   <Switch>
+      //     <Route path="/" exact={true} component={Home} />
+      //     <Route path="/user" exact={true} component={User} />
+      //     <Route path="/detail" exact={true} component={Detail} />
+      //     <Route path="/seat" exact={true} component={Seat} />
+      //   </Switch>
+      // </Router>
+      <div>1</div>
     );
   }
 }
